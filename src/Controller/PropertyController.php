69c4dae4adb69e477d9116ced4771ddd3b05567c
marie101010 <marie.behaegel@hotmail.fr>
2020-05-20 15:24:19 +0200

    ajout divers

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -7,12 +7,14 @@ use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PropertyController extends AbstractController{
 
-    public function __construct(PropertyRepository $repository)
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
     {
-        $this->repository = $repository;
+        $this->repository = $repository ;
+        $this->em = $em;
     }
     /**
      * @Route("/biens",name="property.index")
@@ -20,8 +22,7 @@ class PropertyController extends AbstractController{
      */
     public function index() : Response
         {
-            $property = $this->repository->find (1);
-            dump($property);
+
             return $this->render('property/index.html.twig',[
                 'current_menu' => 'properties'
             ]);
