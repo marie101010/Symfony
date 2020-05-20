49e05dc78073e8cb4dd4001e4f49f03d6743e315
marie101010 <marie.behaegel@hotmail.fr>
2020-05-20 14:26:28 +0200

    avant FE

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,33 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController{
 
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
     /**
      * @Route("/biens",name="property.index")
      * @return Response
      */
     public function index() : Response
         {
-            $property = new Property();
-            $property->setTitle('Mon premier bien')
-                ->setPrice(200000)
-                ->setRooms(4)
-                ->setBedrooms(3)
-                ->setDescription('Une description')
-                ->setSurface(60)
-                ->setFloor(4)
-                ->setHeat(1)
-                ->setCity('Metz')
-                ->setAdress('7 rue de la pierre Hardie')
-                ->setPostalCode('57000');
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($property);
-            $em->flush();
+            $property = $this->repository->find (1);
+            dump($property);
             return $this->render('property/index.html.twig',[
                 'current_menu' => 'properties'
             ]);
